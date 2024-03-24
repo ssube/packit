ab4f3459a850e593cdf7b5a31ab3d2b9cc31c052
Sean Sube <seansube@gmail.com>
2024-03-24 13:17:09 -0500

    only run the first code block in code eval example

--- examples/code_eval.py
+++ examples/code_eval.py
@@ -46,24 +46,29 @@ result = programmer(
 logger.info("Raw result: %s", result)
 
 code_blocks = markdown_result(result)
-for block in code_blocks:
-    logger.info("Code block: %s", block)
-    new_tools = create_python_tool(block)
-    logger.info("New tools: %s", [tool.__name__ for tool in new_tools])
+if len(code_blocks) == 0:
+    logger.error("No code blocks found")
+    exit()
 
-    # Prepare a toolbox with the generated tools
-    toolbox = Toolbox([*new_tools, create_python_tool])
 
-    # Get the factorial of a random number
-    number = randint(1, 100)
-    result = programmer(
-        "Using the function you just wrote, calculate the factorial of {n}. "
-        + get_random_prompt("function"),
-        example=get_function_example(),
-        n=number,
-        tools=toolbox.definitions,
-    )
+block = code_blocks[0]
+logger.info("Code block: %s", block)
+new_tools = create_python_tool(block)
+logger.info("New tools: %s", [tool.__name__ for tool in new_tools])
 
-    logger.info("Raw result: %s", result)
-    factorial = function_result(result, toolbox.callbacks)
-    logger.info("Factorial of %s: %s", number, factorial)
+# Prepare a toolbox with the generated tools
+toolbox = Toolbox([*new_tools, create_python_tool])
+
+# Get the factorial of a random number
+number = randint(1, 100)
+result = programmer(
+    "Call the function you just wrote with a parameter of {n}. "
+    + get_random_prompt("function"),
+    example=get_function_example(),
+    n=number,
+    tools=toolbox.definitions,
+)
+
+logger.info("Raw result: %s", result)
+factorial = function_result(result, toolbox.callbacks)
+logger.info("Factorial of %s: %s", number, factorial)
